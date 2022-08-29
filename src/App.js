00a75e22c2f682d788ac31745ff976c7a5759818
Sameer Shahid <goku.hulk234@gmail.com>
2022-08-29 06:21:32 +0500

    Done Creating Structure and installing Material UI

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
-import React from 'react'
+import React,{useState} from 'react'
+import {MenuItem,FormControl,Select}  from '@mui/material';
 import './App.css'
 
 
 const App = () => {
+const [Countries,setCountries]=useState(['USA','UK','PAKISTAN']);
+
   return (
-    <div>App</div>
-  )
+<div className='app'>
+    <div className="app__header">
+        <h1>COVID-19 TRACKER</h1>
+        <FormControl className='app__dropdown'>
+            <Select variant='outlined' value='abc'>
+               {Countries.map((data)=>(<MenuItem value="worldwide">{data}</MenuItem>))}
+            </Select>
+        </FormControl>          
+    </div>
+        
+       {/*Header*/}
+       {/*Titlte + Select input drop down*/}
+      
+       {/*Info Boxs*/}
+       {/*Info Boxs*/}
+       {/*Info Boxs*/}
+
+       {/*Tables*/}
+       {/*Graph*/}
+
+       {/*Map*/}       
+    </div>
+)
 }
 
 export default App
